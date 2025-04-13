ac462a595f2da816a7af19aa7989c995b4a5226c
fkirchhofer <fkirchhofer@nerve.artorg.unibe.ch>
2025-04-13 08:51:01 +0000

    Got first heatmaps with layer gradcam

---
+++ third_party/__init__.py
@@ -0,0 +1,12 @@
+# third_party/__init__.py
+
+# Import specific functions to make them available directly from the package
+from .run_models import parse_arguments, prepare_data, get_model, load_checkpoint, eval_model
+from .utils import remove_prefix, extract_study_id, compute_accuracy, comput_youden_idx, compute_f1_score
+
+# Define package-level variables
+__version__ = '0.1.0'
+__author__ = 'Fabricio'
+
+# You could also run initialization code here
+#print("Initializing third_party package")
